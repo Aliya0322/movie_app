f2028e677b3d2e0b9078284e07b3367b61549078
Алия Дюсембекова <DusembekovaAZ@gmail.com>
2025-03-29 19:25:27 +0300

    init commit

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,9 +1,28 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-};
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './dist/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+ };
